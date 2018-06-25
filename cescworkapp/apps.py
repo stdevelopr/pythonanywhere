065458ee53cf6ce8e71d5eedff4dacc5c571b6ee
stdevelopr <stdevelopr@gmail.com>
2018-06-25 17:28:36 -0300

    cescworkapp added

---
+++ cescworkapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CescworkappConfig(AppConfig):
+    name = 'cescworkapp'
---
+++ cescworkapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def home(request):
+	return render(request, 'home.html', {})
+
+def work_detail(request, id):
+	return render(request, 'work_detail.html', {'id':id})
+
+def register(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username = username, password=password)
+			login(request, user)
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+	form = UserCreationForm()
+	context = {'form' :form}
+	return render(request, 'registration/register.html', context)
