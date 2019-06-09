36da3918d12133bd5feffe5448ce5466e9bd9faf
stdevelopr <stdevelopr@gmail.com>
2019-06-09 22:52:14 +0000

    portfolio pdf

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 import os
 
-def index(request):       
+def index(request):
 	return render(request, 'portfolio/index.html')
 
 def download(request):
-	response = HttpResponse(content_type='application/pdf')
-	response['Content-Disposition'] = 'attachment; filename="portfolio.pdf"'
-	return response
+    with open('/home/stdevelopr/portfolio.pdf', 'rb') as pdf:
+        path = '/home/stdevelopr/portfolio.pdf'
+        response = FileResponse(open(path, 'rb'), content_type="application/pdf")
+        response['Content-Disposition'] = 'attachment; filename="portfolio.pdf"'
+        return response
 
